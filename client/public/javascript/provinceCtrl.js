fafbe83a38a5a076e0ee81173c856cbd68d05a67
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-16 07:11:44 -0800

    Add functionality to search for a second city

--- client/public/javascript/provinceCtrl.js
+++ client/public/javascript/provinceCtrl.js
@@ -1,8 +1,14 @@
 angular.module('province.ctrl', [])
   .controller('provinceCtrl', function($scope, Cities) {
-    $scope.findCity = function() {
-      Cities.findCity($scope.citySearch)
+    $scope.findCities = function() {
+      Cities.findCity($scope.citySearch1)
         .then(function(res) {
+          $scope.cityData1 = res.data;
+          console.log('in ctrl, res.data: ', res.data);
+        });
+      Cities.findCity($scope.citySearch2)
+        .then(function(res) {
+          $scope.cityData2 = res.data;
           console.log('in ctrl, res.data: ', res.data);
         });
     };
