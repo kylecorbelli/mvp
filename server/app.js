c3404ba93aeba4c14ff0d4ab98203c9493b5d98b
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-15 21:42:24 -0800

    Redefine routing scheme

--- server/app.js
+++ server/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Routes:
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Province' });
+});
+
+app.post('/city', function(req, res) {
+  console.log('post request with req.body:');
+  console.log(req.body);
+  res.send('success');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
