6628311df9f590ffc5b350eb91962c3f7ac1fe7c
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-16 16:41:38 -0800

    Fix new-mexico from new-mixico

--- helpers/states.js
+++ helpers/states.js
@@ -29,7 +29,7 @@ module.exports = {
   nv: 'nevada',
   nh: 'new-hampshire',
   nj: 'new-jersey',
-  nm: 'new-mixico',
+  nm: 'new-mexico',
   ny: 'new-york',
   nc: 'north-carolina',
   nd: 'north-dakota',
