49b77d1b45607544ded966e564206be0d13f03a2
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-02-16 16:39:04 -0800

    Add conditions for displaying loading gif

--- client/public/javascript/provinceCtrl.js
+++ client/public/javascript/provinceCtrl.js
@@ -1,6 +1,10 @@
 angular.module('province.ctrl', [])
   .controller('provinceCtrl', function($scope, Cities) {
+    $scope.showLoading = false;
     $scope.findCities = function() {
+      $scope.showLoading = true;
+      $scope.cityData1 = false;
+      $scope.cityData2 = false;
       Cities.findCity($scope.citySearch1)
         .then(function(res) {
           $scope.cityData1 = res.data;
